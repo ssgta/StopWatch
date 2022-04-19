cef26e70de8946fc1d1b569828121de2ad181a3b
ssgta <62765221+ssgta@users.noreply.github.com>
2022-04-19 23:33:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace StopWatchExercise {
+    class Program {
+        static void Main() {
+            RunStopWatch();
+        }
+
+        private static void RunStopWatch() {
+            var stopWatch = new StopWatch();
+            bool runStopWatch = true;
+
+            while (runStopWatch) {
+                if (stopWatch != null) {
+                    var userChoice = GetUserInput();
+
+                    switch (userChoice) {
+                        case 1:
+                            stopWatch.Start();
+                            break;
+                        case 2:
+                            stopWatch.Stop();
+                            break;
+                        case 3:
+                            stopWatch.Reset();
+                            break;
+                        default:
+                            runStopWatch = false;
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Duration: {stopWatch?.Duration.ToString("hh':'mm':'ss")}");
+        }
+
+        private static int GetUserInput() {
+            Console.Write("Please key a number to: " +
+                "\n 1. Start" +
+                "\n 2. Stop " +
+                "\n 3. Reset" +
+                "\n 4. Exit \n");
+
+            Console.Write("Choice: ");
+            var decision = Console.ReadLine();
+
+            if (decision is not null) {
+                try {
+                    var choice = Int32.Parse(decision);
+
+                    return choice switch {
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        _ => 4
+                    };
+                }
+                catch (Exception) {
+                    Console.WriteLine("Invalid choice. Exiting.");
+                    return 4;
+                }
+            }
+            else {
+                return 4;
+            }
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,28 @@
+﻿namespace StopWatchExercise {
+    class StopWatch {
+        private TimeOnly _startTime;
+        private TimeSpan _duration;
+
+        public TimeSpan Duration {
+            get => _duration;
+            private set => _duration = value;
+        }
+
+        public void Start() {
+            Console.WriteLine("Stopwatch started...\n");
+            _startTime = TimeOnly.FromDateTime(DateTime.Now);
+        }
+
+        public void Stop() {
+            Console.WriteLine("Stopwatch paused...\n");
+            Duration += TimeOnly.FromDateTime(DateTime.Now) - _startTime;
+            _startTime = default;
+        }
+
+        public void Reset() {
+            Console.WriteLine("Stopwatch reset.\n");
+            _startTime = default;
+            Duration = default;
+        }
+    }
+}
